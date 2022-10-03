90d0be37251892836e4a1b2a43b4cc069d249e4e
John <jboyle15@yahoo.com>
2022-10-03 11:49:52 -0500

    event listeners added

--- script.js
+++ script.js
@@ -3,18 +3,30 @@
 const cookie = document.querySelector('.cookie')
 let counter = document.querySelector('.counter')
 let wallet = 0
+
 const upgrade1 = document.querySelector('#upgrade1')
 const upgrade1HTML = document.querySelector('.upgrade1HTML')
+let upgrade1activation = false
+
 const upgrade2 = document.querySelector('#upgrade2')
 const upgrade2HTML = document.querySelector('.upgrade2HTML')
+let upgrade2activation = false
+
 const upgrade3 = document.querySelector('#upgrade3')
 const upgrade3HTML = document.querySelector('.upgrade3HTML')
+let upgrade3activation = false
+
 const upgrade4 = document.querySelector('#upgrade4')
 const upgrade4HTML = document.querySelector('.upgrade4HTML')
+let upgrade4activation = false
+
 const upgrade5 = document.querySelector('#upgrade5')
 const upgrade5HTML = document.querySelector('.upgrade5HTML')
+let upgrade5activation = false
+
 const upgrade6 = document.querySelector('#upgrade6')
 const upgrade6HTML = document.querySelector('.upgrade6HTML')
+let upgrade6activation = false
 
 ////////////////////////////////
 // Functions For Game Logic Here
@@ -24,6 +36,20 @@ const addOne = () => {
   counter.innerHTML = wallet
 }
 
+const addTwo = () => {
+  if (upgrade1activation === true) {
+    wallet += 2
+    counter.innerHTML = wallet
+  }
+}
+
+const addFive = () => {
+  if (upgrade2activation === true) {
+    wallet += 5
+    counter.innerHTML = wallet
+  }
+}
+
 const activateUpgrade1 = () => {
   if (wallet >= 10) {
     console.log('upgrade1 is linked')
@@ -31,6 +57,7 @@ const activateUpgrade1 = () => {
     counter.innerHTML = wallet
     upgrade1HTML.style.background = 'blue'
     upgrade1HTML.innerHTML = 'upgrade one'
+    upgrade1activation = true
   } else {
     alert('not enough funds')
   }
@@ -66,11 +93,17 @@ const activateUpgrade6 = () => {
 ////////////////////////////////
 // Event Listeners Here
 cookie.addEventListener('click', addOne)
+upgrade1HTML.addEventListener('click', addTwo)
 upgrade1.addEventListener('click', activateUpgrade1)
+upgrade2HTML.addEventListener('click', addFive)
 upgrade2.addEventListener('click', activateUpgrade2)
+upgrade3HTML.addEventListener('click', addTen)
 upgrade3.addEventListener('click', activateUpgrade3)
+upgrade4HTML.addEventListener('click', addFifty)
 upgrade4.addEventListener('click', activateUpgrade4)
+upgrade5HTML.addEventListener('click', addHundred)
 upgrade5.addEventListener('click', activateUpgrade5)
+upgrade6HTML.addEventListener('click', autoClicker)
 upgrade6.addEventListener('click', activateUpgrade6)
 
 ////////////////////////////////
